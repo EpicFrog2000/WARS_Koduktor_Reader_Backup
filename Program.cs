ac4aa277bde09a9b018a79a43a51f9ad701ed45a
Norbert Tasarz <norbert.tasarz@iteger.local>
2025-03-05 09:00:42 +0100

    Lepsze oznaczenia typy zakładki

--- Program.cs
+++ Program.cs
@@ -10,8 +10,15 @@ namespace Excel_Data_Importer_WARS
         private static Error_Logger error_logger = new(true); // true - Console write message on creating new error
         private static Config config = new();
         private static readonly bool LOG_TO_TERMINAL = true;
-        
         private static readonly bool Do_Stuff_In_loop = false;
+        private enum Typ_Zakladki
+        {
+            Nierozopznana = -1,
+            Tabela_Stawek = 0,
+            Karta_Ewidencji_Konduktora = 1,
+            Karta_Ewidencji_Pracownika = 2,
+            Grafik_Pracy_Pracownika = 3
+        }
         public static int Main()
         {
             try
@@ -150,25 +157,25 @@ namespace Excel_Data_Importer_WARS
                 Internal_Error_Logger.Nr_Zakladki = Obecny_Numer_Zakladki;
                 IXLWorksheet Zakladka = Workbook.Worksheet(Obecny_Numer_Zakladki);
                 Internal_Error_Logger.Nazwa_Zakladki = Zakladka.Name;
-                int Typ_Zakladki = Get_Typ_Zakladki(Zakladka);
+                Typ_Zakladki Typ_Zakladki = Get_Typ_Zakladki(Zakladka);
                 try
                 {
                     switch (Typ_Zakladki)
                     {
-                        case 2:
+                        case Typ_Zakladki.Tabela_Stawek:
                             Reader_Tabela_Stawek_v1.Process_Zakladka(Zakladka, Internal_Error_Logger);
                             break;
-                        case 3:
+                        case Typ_Zakladki.Karta_Ewidencji_Konduktora:
                             Obecny_Numer_Zakladki = Ilosc_Zakladek_W_Workbook + 1;
                             Reader_Karta_Ewidencji_Konduktora_v1.Process_Zakladka(Zakladka, Internal_Error_Logger);
                             break;
-                        case 4:
+                        case Typ_Zakladki.Karta_Ewidencji_Pracownika:
                             Reader_Karta_Ewidencji_Pracownika.Process_Zakladka(Zakladka, Internal_Error_Logger);
                             break;
-                        case 5:
+                        case Typ_Zakladki.Grafik_Pracy_Pracownika:
                             Reader_Grafik_Pracy_Pracownika_2025_v3.Process_Zakladka(Zakladka, Internal_Error_Logger);
                             break;
-                        default:
+                        case Typ_Zakladki.Nierozopznana:
                             _ = Copy_Bad_Sheet_To_Files_Folder(Workbook.Properties, Zakladka, File_Path);
                             Workbook!.Dispose();
                             Stream!.Close();
@@ -201,7 +208,7 @@ namespace Excel_Data_Importer_WARS
                 Move_File(File_Path, 2);
             }
         }
-        private static int Get_Typ_Zakladki(IXLWorksheet Worksheet)
+        private static Typ_Zakladki Get_Typ_Zakladki(IXLWorksheet Worksheet)
         {
             Stopwatch PomiaryStopWatch = new();
             PomiaryStopWatch.Restart();
@@ -209,35 +216,35 @@ namespace Excel_Data_Importer_WARS
             if (Cell_Value.Contains("Harmonogram pracy"))
             {
                 Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-                return 1;
+                return Typ_Zakladki.Grafik_Pracy_Pracownika;
             }
 
             Cell_Value = Worksheet.Cell(1, 3).GetFormattedString().Trim().Replace("  ", " ");
             if (Cell_Value.Contains("Tabela Stawek"))
             {
                 Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-                return 2;
+                return Typ_Zakladki.Tabela_Stawek;
             }
 
             Cell_Value = Worksheet.Cell(1, 1).GetFormattedString().Trim().Replace("  ", " ");
             if (Cell_Value.Contains("KARTA EWIDENCJI CZASU PRACY"))
             {
                 Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-                return 3;
+                return Typ_Zakladki.Grafik_Pracy_Konduktora;
             }
 
             Cell_Value = Worksheet.Cell(1, 1).Value.ToString();
             if (Cell_Value.Trim().StartsWith("GRAFIK PRACY MIESIĄC")) // grafik v2024 v2
             {
                 Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-                return 5;
+                return Typ_Zakladki.Grafik_Pracy_Pracownika;
             }
 
             Cell_Value = Worksheet.Cell(1, 2).Value.ToString();
             if (Cell_Value.Trim().StartsWith("GRAFIK PRACY MIESIĄC")) // grafik v2024 v2
             {
                 Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-                return 5;
+                return Typ_Zakladki.Grafik_Pracy_Pracownika;
             }
 
             foreach (IXLCell cell in Worksheet.CellsUsed()) // karta pracy NIE konduktora
@@ -247,13 +254,13 @@ namespace Excel_Data_Importer_WARS
                     if (cell.GetString().Trim() == "Dzień")
                     {
                         Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-                        return 4;
+                        return Typ_Zakladki.Karta_Ewidencji_Pracownika;
                     }
                 }
                 catch { }
             }
             Pomiar.Avg_Get_Typ_Zakladki = PomiaryStopWatch.Elapsed;
-            return 0;
+            return Typ_Zakladki.Nierozopznana;
         }
         private static void Usun_Ukryte_Karty(XLWorkbook workbook)
         {
